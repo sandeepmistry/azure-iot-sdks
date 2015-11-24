d7d31029ee926b62a1b9014e29ca02dfaad076e3
Sandeep Mistry <s.mistry@arduino.cc>
2015-11-24 09:15:35 -0500

    Create copy of linux folder for OS X

---
+++ c/build_all/osx/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#set -o pipefail
+#
+
+set -e
+
+script_dir=$(cd "$(dirname "$0")" && pwd)
+build_root=$(cd "${script_dir}/../.." && pwd)
+log_dir=$build_root
+run_unit_tests=ON
+run_e2e_tests=ON
+run_longhaul_tests=OFF
+build_amqp=ON
+build_http=ON
+build_mqtt=ON
+
+usage ()
+{
+    echo "build.sh [options]"
+    echo "options"
+    echo " -cl, --compileoption <value>  specify a compile option to be passed to gcc"
+    echo "   Example: -cl -O1 -cl ..."
+    echo " --skip-e2e-tests              skip the running of end-to-end tests (e2e tests are run by default)"
+    echo " --run-longhaul-tests          run long haul tests (long haul tests are not run by default)"
+    echo ""
+    echo " --no-amqp                     do no build AMQP transport and samples"
+    echo " --no-http                     do no build HTTP transport and samples"
+    echo " --no-mqtt                     do no build MQTT transport and samples"
+    exit 1
+}
+
+process_args ()
+{
+    save_next_arg=0
+    extracloptions=" "
+
+    for arg in $*
+    do      
+      if [ $save_next_arg == 1 ]
+      then
+        # save arg to pass to gcc
+        extracloptions="$arg $extracloptions"
+        save_next_arg=0
+      else
+          case "$arg" in
+              "-cl" | "--compileoption" ) save_next_arg=1;;
+              "--skip-e2e-tests" ) run_e2e_tests=OFF;;
+              "--run-longhaul-tests" ) run_longhaul_tests=ON;;
+              "--no-amqp" ) build_amqp=OFF;;
+              "--no-http" ) build_http=OFF;;
+              "--no-mqtt" ) build_mqtt=OFF;;
+              * ) usage;;
+          esac
+      fi
+    done
+}
+
+process_args $*
+
+rm -r -f ~/cmake
+mkdir ~/cmake
+pushd ~/cmake
+cmake -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt:BOOL=$build_mqtt $build_root
+make --jobs=$(nproc)
+ctest -C "Debug" -V
+popd
---
+++ c/build_all/osx/setup.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#This script updates a fresh Ubuntu installation with all the dependent
+# components necessary to use the IoT Client SDK for C.
+
+repo_name_from_uri()
+{
+    echo "$1" | sed -e 's|/$||' -e 's|:*/*\.git$||' -e 's|.*[/:]||g'
+}
+
+scriptdir=$(cd "$(dirname "$0")" && pwd)
+deps="curl libcurl4-openssl-dev uuid-dev uuid g++ make cmake git unzip openjdk-7-jre libssl-dev"
+repo="https://github.com/Azure/azure-iot-sdks.git"
+repo_name=$(repo_name_from_uri $repo)
+
+push_dir () { pushd $1 > /dev/null; }
+pop_dir () { popd $1 > /dev/null; }
+
+repo_exists ()
+{
+    push_dir "$scriptdir"
+    [ "$(git rev-parse --is-inside-work-tree)" == "true" ] || return 1
+    origin=$(git config remote.origin.url) || return 1
+    actual_name=$(repo_name_from_uri $origin)
+    [ "${repo_name,,}" == "${actual_name,,}" ] || return 1
+    pop_dir
+}
+
+deps_install ()
+{
+    sudo apt-get update
+    sudo apt-get install -y $deps
+}
+
+clone_source ()
+{
+    git clone $repo
+}
+
+install_proton_from_source ()
+{
+    #Skip if already installed
+    code_dir="$HOME/qpid-proton"
+    if [ -d $code_dir ] && [ -d "$code_dir/proton-c" ] && [ -f "$code_dir/build/install_manifest.txt" ]
+    then
+        echo "Proton already installed."
+        return 0
+    fi
+
+    sudo bash c/build_all/linux/build_proton.sh --install /usr
+    [ $? -eq 0 ] || return $?
+}
+
+install_paho_from_source ()
+{
+    #Skip if already installed
+    code_dir="$HOME/org.eclipse.paho.mqtt.c"
+    if [ -d $code_dir ] && [ -d "$code_dir/build/output" ]
+    then
+        echo "Paho already installed."
+        return 0
+    fi
+
+    sudo bash c/build_all/linux/build_paho.sh --install /usr
+    [ $? -eq 0 ] || return $?
+}
+
+deps_install
+
+if repo_exists
+then
+    echo "Repo $repo_name already cloned"
+    push_dir "$(git rev-parse --show-toplevel)"
+else
+    clone_source || exit 1
+    push_dir "$repo_name"
+fi
+
+install_proton_from_source || exit 1
+install_paho_from_source || exit 1
+pop_dir
