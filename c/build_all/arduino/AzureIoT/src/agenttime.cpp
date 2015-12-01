0d3eaea9ad52b31bd7b3133fa2dd00d3b9b2b6c0
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-01 08:56:00 -0500

    Retry NTP fetch 5 times, and use last fetch time + millis for get_time calls within a minute from last fetch

--- c/build_all/arduino/AzureIoT/src/agenttime.cpp
+++ c/build_all/arduino/AzureIoT/src/agenttime.cpp
@@ -1,15 +1,35 @@
 #include "sdk/agenttime.h"
+#include "sdk/iot_logging.h"
 
 #include "util/NTPClient.h"
+
+#define NTP_RETRY_COUNT 5
+#define MAX_CACHE_DELTA 60
+
+long epochTimeFetch = 0;
+uint32_t epochTime = (time_t)-1;
 
 time_t get_time(time_t* currentTime)
 {
     NTPClient ntpClient;
-    uint32_t epochTime;
 
-    ntpClient.begin();
-    epochTime = ntpClient.getEpochTime("time.nist.gov");
-    ntpClient.end();
+    if (epochTime == (time_t)-1 || (((millis() - epochTimeFetch) / 1000) > MAX_CACHE_DELTA)) {
+        ntpClient.begin();
+        for (int i = 0; (i < NTP_RETRY_COUNT) && (epochTime == (uint32_t)-1); i++) {
+            epochTime = ntpClient.getEpochTime("0.pool.ntp.org");
+
+            if (epochTime == (uint32_t)-1) {
+                LogError("Fetching NTP epoch time failed, attempt: %d/%d\n", i, NTP_RETRY_COUNT);
+            } else {
+                LogInfo("epoch time is: %lu\n", epochTime);
+            }
+        }
+        ntpClient.end();
+
+        epochTimeFetch = millis();
+    } else {
+        epochTime += ((millis() - epochTimeFetch) / 1000);
+    }
 
     if (currentTime) {
         *currentTime = epochTime;
